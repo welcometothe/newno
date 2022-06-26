43ac7f7e00aebcc32057a66bb4f3cf34374e19c3
unknown <vaibhavpatil100008@gmail.com>
2022-06-26 23:24:22 +0530

    number progm is done

---
+++ Helloworld.java
@@ -0,0 +1,160 @@
+
+//public class Helloworld{
+//public static void main(String[] args){
+//        System.out.print("*\n**\n***\n****");
+//
+//        }
+//        }
+
+import java.sql.SQLOutput;
+import java.util.*;
+public class Helloworld {
+        public static void main(String[] args) {
+//                Scanner sc =new Scanner(System.in);
+//                int a = sc.nextInt();
+//                int b = sc.nextInt();
+//                int sum = a+b;
+//                System.out.print(sum);
+//                String name = "vaibhav";
+//                int age = 27;
+//                System.out.print(name);
+//
+
+
+                Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//        if(age>18)
+//
+//        {
+//                System.out.println("adult");
+//        }else
+//
+//        {
+//                System.out.println("non Adult");
+//        }
+//}
+//}
+
+//
+//                int a = sc.nextInt();
+//                if (a % 2 == 0) {
+//                        System.out.println("even");
+//                } else {
+//                        System.out.println("odd");
+//                }
+//        }
+//}
+//                int a = sc.nextInt();
+//                int b = sc.nextInt();
+//                if (a == b) {
+//                        System.out.println("no.is equal");
+//                } else {
+//                        if (a > b) {
+//                                System.out.println("a is greater");
+//                        } else {
+//                                System.out.println("Lesser");
+//                        }
+//                }
+//        }
+//                int button = sc.nextInt();
+//                if (button == 1) {
+//                        System.out.println("Hello");
+//                } else if (button == 2) {
+//                        System.out.println("Nameste");
+//                } else if (button == 3) {
+//                        System.out.println("Buzzinga");
+//                } else {.out
+//                        System.out.println("invalid");
+//                }
+//        }
+//}
+
+//                int button = sc.nextInt();
+//                switch (button) {
+//                        case 1:
+//                                System.out.println("Hello");
+//                                break;
+//                        case 2:
+//                                System.out.println("Nameste");
+//                                break;
+//                        case 3:
+//                                System.out.println("buzzinga");
+//                                break;
+//                }
+//        }
+//}
+
+//
+//                int number = sc.nextInt();
+//                switch (number) {
+//                        case 1:
+//                                System.out.println("januray");
+//                                break;
+//                        case 2:
+//                                System.out.println("Feburary");
+//                                break;
+//                        case 3:
+//                                System.out.println("March");
+//                                break;
+//                        case 4:
+//                                System.out.println("april");
+//                                break;
+//                        case 5:
+//                                System.out.println("May");
+//                                break;
+//                        case 6:
+//                                System.out.println("june");
+//                                break;
+//                        case 7:
+//                                System.out.println("jully");
+//                                break;
+//                        case 8:
+//                                System.out.println("august");
+//                                break;
+//                        case 9:
+//                                System.out.println("september");
+//                                break;
+//                        case 10:
+//                                System.out.println("octrmber");
+//                                break;
+//                        case 11:
+//                                System.out.println("november");
+//                                break;
+//                        case 12:
+//                                System.out.println("december");
+//                                break;
+//                        case 13:
+//                                System.out.println("invalid");
+//                                break;
+//                }
+//        }
+//
+//
+//}
+//                int a =2
+//                int b = 4
+//                int Mul = a*b;
+//                System.out.println(Mul);
+//        }
+//                for (int counter = 0; counter < 3; counter++) {
+//                        System.out.println("Hello world");
+//                }
+//        }
+//}
+//                for (int counter = 0; counter < 10; counter++) {
+//                        System.out.println("Hello world");
+//                }
+//        }
+//for(counter =0; counter<11; coun
+
+//                for (int counter=0; counter<11;counter++){
+//                        System.out.println(counter+" ");
+//                }
+//        }
+//}
+                int n = sc.nextInt();
+                for (int i=1; i<=10; i++) {
+                System.out.println(i*n);
+        }
+}
+}
